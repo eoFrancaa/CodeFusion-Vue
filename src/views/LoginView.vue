879caafb4f60d8348e9b621a8cf9162606aba92b
dudugazuforget <dudu99744@gmail.com>
2024-11-29 12:07:53 -0300

    Implementação do passage

---
+++ src/views/LoginView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import '@passageidentity/passage-elements/passage-auth';
+</script>
+
+<template>
+  <h1>Login</h1>
+  <div class="authContainer">
+    <passage-auth
+      app-id="wTaDyCGpZ4ggswFpuio2zXUy"
+    ></passage-auth>
+  </div>
+</template>
+
+<style></style>
