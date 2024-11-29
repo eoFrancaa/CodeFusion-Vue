50d99cf9d3d8f7d66b8a68a8b35f41b82bddb8ff
dudugazuforget <dudu99744@gmail.com>
2024-11-29 09:42:07 -0300

    criação do layout sobre

---
+++ src/views/SobreView.vue
@@ -0,0 +1,72 @@
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+
+    const pessoa = ref([
+      { nome: 'Rafael', },
+      { nome: 'Eduardo', },
+      { nome: 'Leandro', }
+    ]);
+
+    return {
+      pessoa
+    };
+  }
+};
+</script>
+<template>
+  <div class="container-sobre">
+    <div class="sobre-dev">
+      
+      <div class="fotos-dev" v-for="(pessoa, index) in pessoa" :key="index">
+        <p class="nome-dev">{{ pessoa.nome }}</p>
+      </div>
+    </div>
+    <div class="sobre-texto">
+      <div class="text">
+        A equipe CodeFusion desenvolveu este site como parte de um Hackathon realizado no IFC Araquari. Durante o evento, nosso time se dedicou a criar uma solução inovadora, aplicando habilidades técnicas e criatividade para atender aos desafios propostos. Estamos orgulhosos do resultado alcançado e do aprendizado compartilhado ao longo dessa jornada colaborativa.
+      </div>
+    </div>
+  </div>
+</template>
+<style>
+.container-sobre{
+  display: flex;
+  margin-top: 175px;
+  margin-left: 150px;
+  justify-content: space-around;
+}
+.sobre-dev{
+  display: flex;
+  flex-direction: column;
+  gap: 100px;
+}
+.fotos-dev{
+  width: 150px;
+  height: 150px;
+  background-color: #D9D9D9;
+  border-radius: 50%;
+}
+.sobre-texto{
+  border-radius: 30px;
+  width: 501px;
+  height: 601px;
+  background-color: #D9D9D9;
+  color: #000000;
+  margin-top: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.3);
+}
+.text{
+  background-color: #D9D9D9;
+  font-size: 30px;
+  font-family:Verdana, Geneva, Tahoma, sans-serif;
+}
+.descicao-sobre{
+  display: flex;
+}
+</style>
